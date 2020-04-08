9afab10edbb3515b9c07e144b6d29f2d20615c94
Raphi123 <61335398+Raphi123@users.noreply.github.com>
2020-04-08 17:21:42 -0500

    ciudades

--- php/classCiudades.php
+++ php/classCiudades.php
@@ -13,4 +13,34 @@
         {
             
         }
+
+        public function setIdciudad($id_ciudad)
+        {
+            $this->id_ciudad=$id_ciudad;
+        }
+
+        public function getIdciudad()
+        {
+            return $this->id_ciudad;
+        }
+
+        public function setNombreciudad($nombre_ciudad)
+        {
+            $this->nombre_ciudad=$nombre_ciudad;
+        }
+
+        public function getNombreciudad()
+        {
+            return $this->nombre_ciudad;
+        }
+
+        public function setIdpais($id_pais)
+        {
+            $this->id_pais=$id_pais;
+        }
+
+        public function getIdpais()
+        {
+            return $this->id_pais;
+        }
      }
